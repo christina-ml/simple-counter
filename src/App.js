2b03433918f92764c9e079c23ccae1878c7f9029
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-04 10:54:15 -0400

    added this.state and started count at 0. Made an arrow function to put into the button onClick event, that adds one to the count each time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0
+    }
+  }
+
+  /* You have to write it this way using an arrow function */
+  addOne = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div>The count is currently: { this.state.count } </div>
+        <button onClick={ this.addOne }>Add 1 to the count</button>
+      </div>
+    )
+  }
 }
 
 export default App;
